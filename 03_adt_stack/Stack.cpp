0d942ea8b75c1a9ad8e239100455d0c32837b37d
Alyxiee <throatynickel25@outlook.com>
2026-09-14 21:19:28 -0700

    defined bodies of push, peek, isEmpty, size, isFull

--- 03_adt_stack/Stack.cpp
+++ 03_adt_stack/Stack.cpp
@@ -8,8 +8,19 @@
 
 #include "Stack.h"
 
-void Stack::push(int value) {
+#include <iostream>
+#include <ostream>
+
+void Stack::push(const int value) {
     // TODO: add value to the top of data_ (vector has a method for this)
+
+    if (!isFull()) {
+        topIndex++;
+        data[topIndex] = value;
+    } else {
+        std::cout << "Stack is full" << std::endl;
+    }
+
 
 }
 
@@ -17,19 +28,38 @@ void Stack::pop() {
     // TODO: if data_ isn't empty, remove the top element
     if (!isEmpty()) {
         topIndex--;
+        return;
     }
+    std::cout << "Stack is empty" << std::endl;
 }
 
 int Stack::peek() const {
     // TODO: return the top element of data_
     // undefined if empty -- callers should check isEmpty() first. We'll
     // harden this kind of thing later in the course.
+    if (!isEmpty()) {
+        return data[topIndex];
+    }
+    std::cout << "Stack is empty" << std::endl;
+    return -1;
 }
 
 bool Stack::isEmpty() const {
     // TODO: return whether data_ has zero elements
+    if (topIndex == -1) {
+        return true;
+    }
+    return false;
 }
 
 int Stack::size() const {
     // TODO: return how many elements are in data_ (cast to int)
+    return topIndex + 1;
+}
+
+bool Stack::isFull() const {
+    if (topIndex == 101) {
+        return true;
+    }
+    return false;
 }
